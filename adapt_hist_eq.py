84f60d8dbd8087fad9db39f248c5260140a859c6
sparsh-b <bsparshbss@gmail.com>
2022-03-29 17:07:00 +0530

    minor polishings

--- adapt_hist_eq.py
+++ adapt_hist_eq.py
@@ -1,42 +1,57 @@
+import os
+import glob
 import numpy as np
 import cv2
 import plotly.graph_objects as go
 from hist_eq import hist_equalize
 
-img = cv2.imread('adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/0000000000.png')
-#cv2.imshow('original_img', img)
-hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 cell_size = 200
-print('cell_size:', cell_size)
+print('cell_size used for Adaptive Histogram Equalization:', cell_size)
 
-for row in range(0, hsv_img.shape[0], cell_size):
-    first_row_cell = row
-    if (hsv_img.shape[0] - row <= cell_size) and (hsv_img.shape[0] - row != 1):
-        last_row_cell = hsv_img.shape[0]
-    else:
-        last_row_cell = row + cell_size
-    for col in range(0, hsv_img.shape[1], cell_size):
-        first_col_cell = col
-        if (hsv_img.shape[1] - col <= cell_size) and (hsv_img.shape[1] - col != 1):
-            last_col_cell = hsv_img.shape[1]
+def adapt_hist_eq(img_path='adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/0000000000.png', flag=0):
+    img = cv2.imread(img_path)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    for row in range(0, hsv_img.shape[0], cell_size):
+        first_row_cell = row
+        if (hsv_img.shape[0] - row <= cell_size) and (hsv_img.shape[0] - row != 1):
+            last_row_cell = hsv_img.shape[0]
         else:
-            last_col_cell = col + cell_size
-        mapping = hist_equalize(hsv_img[first_row_cell:last_row_cell, first_col_cell:last_col_cell, 2])
-        for i in range(first_row_cell, last_row_cell):
-            for j in range(first_col_cell, last_col_cell):
-                hsv_img[i,j,2] = mapping[hsv_img[i,j,2]]
+            last_row_cell = row + cell_size
+        for col in range(0, hsv_img.shape[1], cell_size):
+            first_col_cell = col
+            if (hsv_img.shape[1] - col <= cell_size) and (hsv_img.shape[1] - col != 1):
+                last_col_cell = hsv_img.shape[1]
+            else:
+                last_col_cell = col + cell_size
+            mapping = hist_equalize(hsv_img[first_row_cell:last_row_cell, first_col_cell:last_col_cell, 2])
+            for i in range(first_row_cell, last_row_cell):
+                for j in range(first_col_cell, last_col_cell):
+                    hsv_img[i,j,2] = mapping[hsv_img[i,j,2]]
 
-print('Standard Deviation of the HSV adaptive histogram equalized scene:', np.std(hsv_img[:,:,2]))
-unique_vals, counts = np.unique(hsv_img[:,:,2], return_counts=True)
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=unique_vals, y=counts))
-fig.update_layout(title='adapt_hist_eq', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
-fig.show()
+    bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+    if flag==0:
+        cv2.imwrite('adapt_hist_eq.jpg', bgr_img)
+        print('Standard Deviation of the adaptive histogram equalized scene for 1st frame:', np.std(hsv_img[:,:,2]))
+        unique_vals, counts = np.unique(hsv_img[:,:,2], return_counts=True)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=unique_vals, y=counts))
+        fig.update_layout(title='adapt_hist_eq', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
+        fig.show()
+    else:
+        if not os.path.exists(os.path.join('.','results','adapt_hist_eq')):
+            os.makedirs(os.path.join('.','results','adapt_hist_eq'))
+        cv2.imwrite(os.path.join('.','results','adapt_hist_eq',img_path.split('/')[-1]), bgr_img)
 
-bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-#cv2.imshow('hsv_adapt_eq', bgr_img)
-cv2.imwrite('hsv_adapt_eq.jpg', bgr_img)
-cv2.waitKey(0)
+def adapt_hist_eq_all():
+    imgs = glob.glob('adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/*.png')
+    for img in imgs:
+        adapt_hist_eq(img, 1)
 
-print('Adaptive histogram Equalized image is saved to current directory as: hsv_adapt_eq.jpg')
+if __name__ == '__main__':
+    adapt_hist_eq()
+    adapt_hist_eq_all()
+
+    print('Adaptive histogram Equalized image for 1st frame is saved to current directory as: adapt_hist_eq.jpg')
+    print('Intensities of Adaptive histogram equalized scene for 1st frame are displayed in browser!')
+    print('\nAdaptive histogram Equalized images for the entire dataset is stored at `./results/adapt_hist_eq`!')
--- hist_eq.py
+++ hist_eq.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import numpy as np
 import cv2
 import plotly.graph_objects as go
@@ -28,33 +30,42 @@ def hist_equalize(v):
     mapping = dict(zip(histEq_x, histEq_y))
     return mapping
 
-if __name__ == '__main__':
-    img = cv2.imread('adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/0000000000.png')
+def hsv_hist_equalize(img_path='adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/0000000000.png', flag=0):
+    img = cv2.imread(img_path)
     cv2.imshow('original_img', img)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     v = hsv_img[:,:,2]
-    print('Standard Deviation of the original scene:', np.std(v))
-    unique_vals, counts = np.unique(v, return_counts=True)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=unique_vals, y=counts))
-    fig.update_layout(title='original_distn', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
-    fig.show()
+    if flag==0:
+        print('Standard Deviation of the original scene for 0th frame:', np.std(v))
+        unique_vals, counts = np.unique(v, return_counts=True)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=unique_vals, y=counts))
+        fig.update_layout(title='original_distn', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
+        fig.show()
 
     mapping = hist_equalize(v)
 
     for row in range(hsv_img.shape[0]):
         for col in range(hsv_img.shape[1]):
             hsv_img[row, col, 2] = mapping[hsv_img[row, col, 2]]
-    print('Standard Deviation of the HSV equalized scene:', np.std(hsv_img[:,:,2]))
-    unique_vals, counts = np.unique(hsv_img[:,:,2], return_counts=True)
-    bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=unique_vals, y=counts))
-    fig.update_layout(title='hist_equalization', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
-    fig.show()
-    cv2.imshow('hsv_equalization', bgr_img)
-    cv2.imwrite('hsv_equalization.jpg', bgr_img)
+    bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)    
 
+    if flag==0:
+        cv2.imwrite('hsv_equalization.jpg', bgr_img)
+        print('Standard Deviation of the HSV equalized scene for 0th frame:', np.std(hsv_img[:,:,2]))
+        unique_vals, counts = np.unique(hsv_img[:,:,2], return_counts=True)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=unique_vals, y=counts))
+        fig.update_layout(title='hist_equalization', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
+        fig.show()
+    else:
+        if not os.path.exists(os.path.join('.','results','hist_eq')):
+            os.makedirs(os.path.join('.','results','hist_eq'))
+        cv2.imwrite(os.path.join('.','results','hist_eq',img_path.split('/')[-1]), bgr_img)
+    
+
+def rgb_hist_equalize():
+    img = cv2.imread('adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/0000000000.png')
     b = img[:,:,0]
     g = img[:,:,1]
     r = img[:,:,2]
@@ -66,15 +77,27 @@ if __name__ == '__main__':
             img[row, col, 0] = mapping_b[img[row, col, 0]]
             img[row, col, 1] = mapping_g[img[row, col, 1]]
             img[row, col, 2] = mapping_r[img[row, col, 2]]
-    cv2.imshow('bgr_equalization', img)
+    #cv2.imshow('bgr_equalization', img)
     cv2.imwrite('bgr_equalization.jpg', img)
     hsv_img_rgb_eq = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     v_rgb_eq = hsv_img_rgb_eq[:,:,2]
-    print('Standard Deviation of the RGB equalized scene:', np.std(v_rgb_eq))
+    print('Standard Deviation of the RGB equalized scene for 0th frame:', np.std(v_rgb_eq))
     unique_vals, counts = np.unique(v_rgb_eq, return_counts=True)
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=unique_vals, y=counts))
     fig.update_layout(title='rgb_equalization', title_x=0.5, xaxis_title='pixel value', yaxis_title='counts', font=dict(size=28))
     fig.show()
+    #cv2.waitKey(0)
 
-    cv2.waitKey(0)
+def hist_equalize_all():
+    imgs = glob.glob('adaptive_hist_data-20220320T151318Z-001/adaptive_hist_data/*.png')
+    for img in imgs:
+        hsv_hist_equalize(img, 1)
+
+if __name__ == '__main__':
+    hsv_hist_equalize()
+    rgb_hist_equalize()
+    hist_equalize_all()
+    print('\nrgb vs hsv equalized outputs for 1st frame are written to current directory!')
+    print('Intensities of original, rgb & hsv equalized scenes for 1st frame are displayed in browser!')
+    print('Results for the entire datset are written to `./results/hist_eq`!')
